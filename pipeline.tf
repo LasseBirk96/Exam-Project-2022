a6d99d6068dad0e80af32939fdcbdc873d2d388e
Lasse Birk <lassebirk.birk9@gmail.com>
2022-10-27 15:40:59 +0200

    fixed rookie mistake

--- pipeline.tf
+++ pipeline.tf
@@ -1,5 +1,5 @@
 resource "aws_codebuild_project" "tf-plan" {
-  name          = "tf-cicd-plan2"
+  name          = "tf-cicd-plan"
   description   = "Plan stage for terraform"
   service_role  = aws_iam_role.tf-codebuild-role.arn
 
@@ -70,7 +70,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             output_artifacts = ["tf-code"]
             configuration = {
                 FullRepositoryId = "LasseBirk96/Exam-Project-2022"
-                BranchName   = "master"
+                BranchName   = "main"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
             }
