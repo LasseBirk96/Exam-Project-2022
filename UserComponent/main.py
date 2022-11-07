db682526497ece8894e0909c81a1645ccba60a01
Lasse Birk <lassebirk.birk9@gmail.com>
2022-11-07 13:21:11 +0100

    Terraform is a pain

--- UserComponent/main.py
+++ UserComponent/main.py
@@ -3,3 +3,4 @@ from LogicLayer.Entities.User import User
 
 u = User(1,"Lasse","Birk","gheheh",12,"Lassebirk@gmail.com",12121212)
 
+print("#egf")
--- tf/pipeline.tf
+++ tf/pipeline.tf
@@ -72,7 +72,7 @@ resource "aws_codepipeline" "cicd_pipeline" {
             version = "1"
             output_artifacts = ["tf-code"]
             configuration = {
-                FullRepositoryId = "LasseBirk96/Exam-Project-2022"
+                FullRepositoryId = "LasseBirk96/Exam-Project-2022/tf"
                 BranchName   = "main"
                 ConnectionArn = var.codestar_connector_credentials
                 OutputArtifactFormat = "CODE_ZIP"
