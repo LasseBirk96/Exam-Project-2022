a72e69d9546d116fa76c8245de12e8c86647cab6
Lasse Birk <lassebirk.birk9@gmail.com>
2022-11-07 17:35:40 +0100

    <html> <head><title>301 Moved Permanently</title></head> <body> <center><h1>301 Moved Permanently</h1></center> </body> </html>

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 services=("BankingComponent" "FoodComponent")
 
-for service in $services
+for service in "${services[@]}"
 do
     s=$(echo "$service" | tr '[:upper:]' '[:lower:]')
     image="ghcr.io/lassebirk96/$s:latest"
-    docker build -t $image . --build-arg COMPONENT_PATH=$service
-    docker push $image
+    echo $image
+    #docker build -t $image . --build-arg COMPONENT_PATH=$service
+    #docker push $image
 done
