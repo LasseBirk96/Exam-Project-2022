d4732eb0c8a7faaadaa9a646a150dfd4fbb0a0d5
Lasse Birk <lassebirk.birk9@gmail.com>
2022-11-07 14:36:03 +0100

    Started on user component

---
+++ UserComponent/DataBaseLayer/Setup/postgres_setup.py
@@ -0,0 +1,8 @@
+from UserComponent.DataBaseLayer.Setup import table_setup
+
+def run_setup():
+
+    #SETS UP THE TABLE
+    table_setup.setup_user_table()
+
+
---
+++ UserComponent/DataBaseLayer/Setup/table_setup.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append("..")
+from ..Connection.connector import establish_connection
+
+
+def set_up_table(command):
+    conn = establish_connection()
+    cur = conn.cursor()
+    try:
+        cur.execute(command)
+        cur.close()
+        conn.commit()
+        print("Successfully created user table")
+    except (Exception) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+# Returns the sql query that creates a bank_account table
+def return_table():
+    return  """
+    CREATE TABLE IF NOT EXISTS users (
+    email VARCHAR(255) primary key unique,
+	account_number VARCHAR(255) not NULL,
+    CVV VARCHAR(255) not NULL,
+    pin_code VARCHAR(255) not NULL,
+    balance SMALLINT not NULL
+    );
+    """
+
+
+def setup_user_table():
+    set_up_table(return_table())
