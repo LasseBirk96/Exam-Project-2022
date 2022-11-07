2c4e69ad14a5ef17c3b3625145ae4cc14ebc5179
Lasse Birk <lassebirk.birk9@gmail.com>
2022-11-07 17:36:38 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ for service in "${services[@]}"
 do
     s=$(echo "$service" | tr '[:upper:]' '[:lower:]')
     image="ghcr.io/lassebirk96/$s:latest"
-    echo $image
-    #docker build -t $image . --build-arg COMPONENT_PATH=$service
-    #docker push $image
+    docker build -t $image . --build-arg COMPONENT_PATH=$service
+    docker push $image
 done
