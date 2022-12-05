1b5962feda03bbe94417b40fbcb9a749da5f1c1c
Lasse Birk <lassebirk.birk9@gmail.com>
2022-12-05 14:00:49 +0100

    Testing pipeline 9

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ services=("UserComponent" "ProductComponent")
 for service in "${services[@]}"
 do
   chmod -x "$service/tests.py"
-  python3 -c pytest "$service/tests.py"
+  python -m pytest "$service/tests.py"
 done
